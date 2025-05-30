9ace33cc04744397d27773402e146b0cd5cd764c
Jean-Bernard Hayet <jbhayet@cimat.mx>
2025-05-29 19:13:53 -0600

    Update cdmx for including week day and holiday flag

--- cdmx_generate_series.py
+++ cdmx_generate_series.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import pandas as pd # type: ignore
-
+import holidays # type: ignore
 
 cdmx_flow_file = 'data/cdmx_data_flow.csv'
 if not os.path.exists(cdmx_flow_file):
@@ -11,3 +11,10 @@ if not os.path.exists(cdmx_flow_file):
 # Read the file and print the first 10 lines
 rawDataAll = pd.read_csv(cdmx_flow_file)
 print(f"--- Total number of records: {len(rawDataAll)}")
+print(rawDataAll.head(60))
+
+# A date at 1st of january 2022
+start_date = pd.to_datetime('2022-01-01')
+# Test if the start date is a holiday
+if start_date in holidays.Mexico():
+    print(f"{start_date.date()} is a holiday in Mexico.")
